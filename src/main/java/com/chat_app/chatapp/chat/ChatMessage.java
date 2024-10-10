06d533456c484c1e929755bd6a2ac04997aab985
TBanda27 <tzbanda@outlook.com>
2024-10-10 22:26:20 +0200

    first commit

---
+++ src/main/java/com/chat_app/chatapp/chat/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.chat_app.chatapp.chat;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
---
+++ src/main/java/com/chat_app/chatapp/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.chat_app.chatapp.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
